57e1f4035258d131e8ed7e0e976067b24152d25a
Chris Tsiaousis <chris.tsiaousis@streamhpc.com>
2026-03-17 08:57:21 +0000

    Made sparse amdgcn structs depict the actual builtin signature (halved A vector size)
    Signed-off-by: Chris Tsiaousis <chris.tsiaousis@streamhpc.com>

--- projects/composablekernel/include/ck_tile/core/arch/mma/amdgcn_mma.hpp
+++ projects/composablekernel/include/ck_tile/core/arch/mma/amdgcn_mma.hpp
@@ -137,7 +137,8 @@ template <typename ADataType_,
           index_t kCMPerLane_,
           index_t kCMNumAccess_,
           typename OpType_,
-          MmaOpFamily OpFamily_>
+          MmaOpFamily OpFamily_,
+          index_t kCompressionRatio_ = 1>
 struct amdgcn_mma_base
 {
     using OpType                          = OpType_;
@@ -154,16 +155,17 @@ struct amdgcn_mma_base
     static constexpr index_t kK = FragK; // K = K2 * K1 * K0
 
     // Layout constants, check description above.
-    static constexpr index_t kABKPerLane  = kABKPerLane_;  // K2 * K0
-    static constexpr index_t kAKNumAccess = kAKNumAccess_; // K2
-    static constexpr index_t kARepeat     = kARepeat_;     // RDNA3 repetition and MFMA block-hiding
-    static constexpr index_t kBKNumAccess = kBKNumAccess_; // K2
-    static constexpr index_t kBRepeat     = kBRepeat_;     // RDNA3 repetition and MFMA block-hiding
-    static constexpr index_t kCMPerLane   = kCMPerLane_;   // M2 * M0
-    static constexpr index_t kCMNumAccess = kCMNumAccess_; // M2
+    static constexpr index_t kABKPerLane       = kABKPerLane_;       // K2 * K0
+    static constexpr index_t kAKNumAccess      = kAKNumAccess_;      // K2
+    static constexpr index_t kARepeat          = kARepeat_;          // RDNA3 repetition and MFMA block-hiding
+    static constexpr index_t kBKNumAccess      = kBKNumAccess_;      // K2
+    static constexpr index_t kBRepeat          = kBRepeat_;          // RDNA3 repetition and MFMA block-hiding
+    static constexpr index_t kCMPerLane        = kCMPerLane_;        // M2 * M0
+    static constexpr index_t kCMNumAccess      = kCMNumAccess_;      // M2
+    static constexpr index_t kCompressionRatio = kCompressionRatio_; // Sparse intrisics matrix A compression
 
     // K-dimension compression ratio for A matrix, always 2 for sparse intrinsics.
-    static constexpr index_t kCompressionRatio = (OpFamily == MmaOpFamily::SPARSE) ? 2 : 1;
+    // static constexpr index_t kCompressionRatio = (OpFamily == MmaOpFamily::SPARSE) ? 2 : 1;
 
     // Layout checks
     static_assert(kK % kABKPerLane == 0);
