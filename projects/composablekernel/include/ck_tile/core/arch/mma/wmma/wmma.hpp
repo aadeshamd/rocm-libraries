1b3cb94808f3c0cd6b72a859f9ac76e412812080
Yung-sheng Tu <yung-sheng@streamhpc.com>
2026-04-02 08:29:35 +0000

    move WmmaCtrlFlags and DefaultWmmaCtrlFlags to wmma_traits.hpp

--- projects/composablekernel/include/ck_tile/core/arch/mma/wmma/wmma.hpp
+++ projects/composablekernel/include/ck_tile/core/arch/mma/wmma/wmma.hpp
@@ -3,41 +3,6 @@
 
 #pragma once
 
-namespace ck_tile::core::arch::mma {
-
-/**
- * @enum WmmaCtrlFlags
- * @brief Common wmma control flags for gfx11 and gfx12
- */
-enum struct WmmaCtrlFlags : bool
-{
-    // Only has an effect on gfx11 when the accumulator is 16-bit
-    // Determines which half of the 32-bit accum register to use
-    // Low = bits [15:0]
-    // High = bits[31:16]
-    LOW  = false,
-    HIGH = true,
-};
-
-/**
- * @class DefaultWmmaFlags
- * @brief Generates default WMMA control flags based on data types.
- * @tparam ADataType Data type of matrix A
- * @tparam BDataType Data type of matrix B
- * @tparam CDataType Data type of the accumulator
- */
-template <typename ADataType, typename BDataType, typename CDataType>
-struct DefaultWmmaCtrlFlags
-{
-    constexpr static bool Clamp = false;
-
-    // Generate default flags for accumulator destination bits.
-    // Only used if accumulation size is 16-bit in gfx11
-    constexpr static WmmaCtrlFlags AccumBits = WmmaCtrlFlags::LOW;
-};
-
-} // namespace ck_tile::core::arch::mma
-
 // Include the architecture-specific WMMA implementations and traits
 #include "wmma_gfx11.hpp"
 #include "wmma_gfx12.hpp"
