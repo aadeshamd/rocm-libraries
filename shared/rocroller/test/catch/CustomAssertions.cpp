fd5ba9dfe8777a6dd71cd28f3b7093e9d19c798a
Chun-Xun Lin <Chun-Xun.Lin@amd.com>
2026-04-07 23:41:32 +0000

    Try MRV

--- shared/rocroller/test/catch/CustomAssertions.cpp
+++ shared/rocroller/test/catch/CustomAssertions.cpp
@@ -32,7 +32,8 @@ namespace rocRollerTests
     {
         Catch::Tag tag(tagName);
 
-        auto testName = Catch::getResultCapture().getCurrentTestName();
+        auto testName = "TT";
+        //auto testName = Catch::getResultCapture().getCurrentTestName();
 
         auto infos = getSortedTestInfos();
 
